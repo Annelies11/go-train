eedab8d8a05d251880a14d71fbd8b3b3b82fbc98
Annelies11 <mmahmudi1103@gmail.com>
2024-01-20 10:52:46 +0700

    First commit

---
+++ number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Number 1 : ", 1)
+	fmt.Println("Number two : ", 2.5)
+	fmt.Println("Number : ", 0x123a)
+}
---
+++ var.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Aris Mahmudi"
+	fmt.Println(name)
+	name = "Halo semuanya"
+	fmt.Println(name)
+}
